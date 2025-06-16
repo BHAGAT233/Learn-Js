6b5d858adb6b5063345e3ea16462f448d6a5e7ba
BHAGAT233 <bhagatrammandavi24@navgurukul.org>
2025-06-16 11:14:02 +0530

    new file date_object.js

---
+++ 22.date_Object.js
@@ -0,0 +1,6 @@
+// let currentDate = new Date();
+// console.log(currentDate);
+
+let a = new Date();
+
+console.log(`DATE: ${a.getDate()}/ ${a.getMonth()+1}/ ${a.getFullYear()} time ${a.getHours()}:${a.getMinutes()}:${a.getSeconds()}`);
