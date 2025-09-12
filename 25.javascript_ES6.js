3b3663a095e062161b2b73714e8e8a1dbb38a788
Bantu <bhagatrammandavi24@navgurukul.org>
2025-09-12 12:56:00 +0530

    added new code at ES6 rareted to ES6 and Object

--- 25.javascript_ES6.js
+++ 25.javascript_ES6.js
@@ -50,15 +50,15 @@
 
 //------------------------------------------------------------------------------//
 
-let person = {
-  name: "Bantu",
-  age: 19,
-  address: {
-    city: "Bastar",
-    state: "Chhattisgarh",
-    pincode: 494001
-  }
-};
+// let person = {
+//   name: "Bantu",
+//   age: 19,
+//   address: {
+//     city: "Bastar",
+//     state: "Chhattisgarh",
+//     pincode: 494001
+//   }
+// };
 // console.log(person["address"]["city"]); // "Bastar"
 // for (let key in person) {
 //   console.log(key, ":", person[key]);
@@ -68,5 +68,78 @@ let person = {
 
 //---------------------------------------------------------------------------------//
 
+let NavGrukulData = [
+  { name: 'bantu', age: 19, city: 'bastar' },
+  { name: 'dhurwa', age: 20, city: 'bijapur' },
+  { name: 'sam', age: 18, city: 'dheli' },
+];
+
+// console.log(NavGrukulData);
+
+// console.log(NavGrukulData[0]);
+// Pehla student object: {name: "bantu", age: 19, city: "bastar"}
+
+// console.log(NavGrukulData[1]); 
+// Dusra student object: {name: "dhurwa", age: 20, city: "bijapur"}
+
+// console.log(NavGrukulData[0].name); // "bantu"
+// console.log(NavGrukulData[1].city); // "bijapur"
+// console.log(NavGrukulData[2].age);  // 18
+
+// for (let i = 0; i < NavGrukulData.length; i++) {  // normal for loop
+//   console.log(NavGrukulData[i].name);
+// }
+
+// for (let student of NavGrukulData) {  // for of loop
+//   console.log(student.name, student.age);
+// }
+
+// NavGrukulData.forEach(student => {  // forEach loop It's will show all Data ;
+//   console.log(`Name: ${student.name}, Age: ${student.age}, City: ${student.city}`);
+// });
+
+// let bastarWale = NavGrukulData.filter(s => s.city === "bastar");  // filter with print spacpic Data
+// console.log(bastarWale);
+
+// let samData = NavGrukulData.find(s => s.name === "sam"); // with find() method print spacpic Data 
+// console.log(samData); 
 
 
+//--------------------------------------------------------------------------------------------//
+
+// with function practice Object question
+
+// tumhara data
+// let NGData = [
+//   { name: 'bantu', age: 19, city: 'bastar' },
+//   { name: 'dhurwa', age: 20, city: 'bijapur' },
+//   { name: 'sam', age: 18, city: 'dheli' },
+// ];
+
+// // function jo har student ka intro print kare
+// const printIntroCards = (students) => {
+//   students.forEach(student => {
+//     console.log(`--------------------`);
+//     console.log(`Name: ${student.name}`);
+//     console.log(`Age: ${student.age}`);
+//     console.log(`City: ${student.city}`);
+//     console.log(`--------------------\n`);
+//   });
+// };
+
+// // function call karo
+// printIntroCards(NGData);
+
+//---------------------------------------------------------------------------------------------//
+
+// destructuring in Object
+
+// const person = {
+//   name: "Bantu",
+//   age: 19,
+//   city: "Bastar"
+// };
+
+// const {city} = person;
+
+// console.log(city);
