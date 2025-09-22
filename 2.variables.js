56d7bf5600969505d80217aa64d1f7c2d10f21fc
BHAGAT233 <bhagatrammandavi24@navgurukul.org>
2025-09-22 16:27:54 +0530

    addede new code

--- 2.variables.js
+++ 2.variables.js
@@ -5,13 +5,13 @@ a = "harry"
 console.log(a);
 // let 8harry = 7
 
-// let n = 7;
-// let arr = [1,2,5,7,3,8,6];
-// let i = 0;
-// while (i<n){
-//    if (arr[i]%3 == 0){
-//        console.log(i);
-//     //    break;
-//    }
-//    i = i + 1;
-// }
+let n = 7;
+let arr = [1,2,5,7,3,8,6];
+let i = 0;
+while (i<n){
+   if (arr[i]%3 == 0){
+       console.log(i);
+    //    break;
+   }
+   i = i + 1;
+}
